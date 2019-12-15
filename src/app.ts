ea140047fc2dd04fd6531e308d81063cf9a6393a
Kirill Galkin <kirill.galkin.d@gmail.com>
2019-12-15 22:54:49 +0300

    tasks 03.01 - 03.03 completed

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ enum Category {
 }
 
 interface IBook {
+  id: number;
   title: string;
   author: string;
   available: boolean;
@@ -23,24 +24,28 @@ interface IBook {
 const getAllBooks = (): readonly IBook[] => {
   const books = <const>[
     {
+      id: 1,
       title: "Refactoring JavaScript",
       author: "Evan Burchard",
       available: true,
       category: Category.JavaScript
     },
     {
+      id: 2,
       title: "JavaScript Testing",
       author: "Liang Yuxian Eugene",
       available: false,
       category: Category.JavaScript
     },
     {
+      id: 3,
       title: "CSS Secrets",
       author: "Lea Verou",
       available: true,
       category: Category.CSS
     },
     {
+      id: 4,
       title: "Mastering JavaScript Object-Oriented Programming",
       author: "Andrea Chiarelli",
       available: true,
@@ -50,7 +55,9 @@ const getAllBooks = (): readonly IBook[] => {
   return books;
 };
 
-const logFirstAvailable = (booksArr: readonly IBook[]): void => {
+const logFirstAvailable = (
+  booksArr: readonly IBook[] = getAllBooks()
+): void => {
   const booksQuantity = booksArr.length;
   const firstAvailable = booksArr.find(el => el.available).title;
 
@@ -60,7 +67,9 @@ const logFirstAvailable = (booksArr: readonly IBook[]): void => {
 
 logFirstAvailable(getAllBooks());
 
-const getBookTitlesByCategory = (category: Category): string[] => {
+const getBookTitlesByCategory = (
+  category: Category = Category.JavaScript
+): string[] => {
   const books = getAllBooks();
 
   const booksByCategory = books
@@ -101,3 +110,55 @@ const calcTotalPages = (): bigint => {
 
   return pagesQuantity;
 };
+
+// Task 03.01
+
+// const titles = getBookTitlesByCategory(Category.JavaScript);
+titles.forEach(el => console.log(el));
+
+const getBookById = (id: number): IBook => {
+  const books = getAllBooks();
+  return books.find(book => book.id === id);
+};
+
+getBookById(1);
+
+// Task 03.02
+
+const createCustmerID = (name: string, id: number): string => {
+  return name + id;
+};
+const myID: string = createCustmerID("Ann", 10);
+
+let idGenerator: (name: string, id: number) => string;
+idGenerator = (name: string, id: number) => id + name;
+idGenerator = createCustmerID;
+
+// Task 03.03
+
+const createCustomer = (name: string, age?: number, city?: string): void => {
+  console.log(name);
+  age && console.log(age);
+  city && console.log(city);
+};
+
+createCustomer("Ann");
+createCustomer("Ann", 20);
+createCustomer("Ann", 20, "Moscow");
+
+getBookTitlesByCategory();
+
+const checkoutBooks = (customer: string, ...bookIDs: number[]): string[] => {
+  console.log(customer);
+  const titles: string[] = [];
+  bookIDs.forEach(
+    el =>
+      getBookById(el) &&
+      getBookById(el).available &&
+      titles.push(getBookById(el).title)
+  );
+
+  return titles;
+};
+
+const myBooks = checkoutBooks("Ann", 1, 2, 4);
